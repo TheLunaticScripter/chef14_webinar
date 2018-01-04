4d22ecb1f7bfdbf8f9d4cde3d6c25e03d8e4f711
John Snow <thelunaticscripter@outlook.com>
2018-01-04 13:44:51 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef14_webinar
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef14_webinar
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
